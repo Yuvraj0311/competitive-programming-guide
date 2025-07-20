7d620b21cc6b84de45ed534c69952855d095e645
Yuvraj0311 <yuvrajsingh.uv0311@gmail.com>
2025-07-20 17:18:03 +0530

    Add Snippets folder with starter C++ files

---
+++ Snippets/start.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Shortcuts
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define all(x) (x).begin(), (x).end()
+#define fr(i, a, b) for (int i = a; i < b; ++i)
+#define rep(i, n) for (int i = 0; i < n; ++i)
+#define pb push_back
+#define F first
+#define S second
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve() {
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t = 1;
+    // cin >> t; // Uncomment for multiple test cases
+    while (t--) solve();
+    return 0;
+}
